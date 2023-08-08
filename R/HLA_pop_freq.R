741ff1864aba87c0fc0a733353ae875fafc495b5
Sami-Kanaan <skanaan@fredhutch.org>
2023-08-07 23:16:28 -0700

    version 0.0.2

---
+++ R/HLA_pop_freq.R
@@ -0,0 +1,16 @@
+#' HLA allele frequency in the Human Population
+#'
+#' Data from the Allele Frequency Net Database (AFND) of gene frequencies in different worldwide populations.
+#' The data has been downloaded from the AFND website (http://www.allelefrequencies.net/hla.asp) for classical and non classical HLA alleles; and is up to date as of 13-JUL-2023.
+#'
+#' @docType data
+#'
+#' @usage scrHLAmatrix::freq
+#'
+#' @format An object of class "data.frame".
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' hla_pop_freq <- scrHLAmatrix::freq
+"freq"
