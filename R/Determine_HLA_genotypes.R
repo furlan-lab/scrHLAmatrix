2135ea8fd017b5a13627c65faaf9ccc3a0796ebd
Sami-Kanaan <skanaan@fredhutch.org>
2024-06-20 17:06:33 -0700

    ablity to correct alleles

--- R/Determine_HLA_genotypes.R
+++ R/Determine_HLA_genotypes.R
@@ -145,7 +145,7 @@ Top_HLA_list <- function(reads_1, reads_2 = NULL, allogeneic_entities = 2, seu =
       }
       if (correct_alleles) {
         message(cat(crayon::red("\nWarning: "), "argument 'correct_alleles' is ",
-                    crayon::green("TRUE"), "; replacing the unlikely allele(s) with their 'correct' version(s).", sep = ""))
+                    crayon::green("TRUE"), "; replacing the unlikely allele(s) with their suspected 'correct' version(s).", sep = ""))
         top_alleles_HLA <- sapply(top_alleles_HLA, function(x) if (x %in% is_the_allele_correct) names(is_the_allele_correct)[is_the_allele_correct == x] else x)
         names(top_alleles_HLA) <- NULL
         top_alleles_HLA <- top_alleles_HLA %>% sort()
