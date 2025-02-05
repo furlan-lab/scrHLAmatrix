91980ec4b0675b5029cbaa7bffb1f721c3dcb89c
Sami-Kanaan <skanaan@fredhutch.org>
2025-02-05 13:30:14 -0800

    glitch fix in writing top alleles

--- R/Plot_Top_HLA_alleles.R
+++ R/Plot_Top_HLA_alleles.R
@@ -153,11 +153,7 @@ Top_HLA_plot_byCB <- function(reads, seu = NULL, CB_rev_com = FALSE, hla_with_co
   }
   ## removing HLA alleles with low counts overall
   r <- hla_with_counts_above
-  if (dim(part_HLA[which(rowSums(part_HLA)>=r), , drop = F])[1] < 2) { # must keep at least 2 rows in the matrix
-    part_HLA <- part_HLA[order(-rowSums(part_HLA))[1:2], , drop = F]
-  } else {
-    part_HLA <- part_HLA[which(rowSums(part_HLA)>=r), , drop = F]
-  }
+  part_HLA <- part_HLA[which(rowSums(part_HLA)>=r), , drop = F]
   ## removing cell barcodes (CBs) with low counts overall
   n <- CBs_with_counts_above
   if (dim(part_HLA[,which(colSums(part_HLA)>=n), drop = F])[2] < dim(part_HLA)[1]) {
